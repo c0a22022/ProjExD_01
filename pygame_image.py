978ef7e2d190b0036ceb1fb1e6b911f9162a2e3e
Tomoki Ushiyama <c0a22022ae@edu.teu.ac.jp>
2023-04-18 17:30:57 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -6,10 +6,13 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img_2 = pg.transform.rotozoom(kk_img,10,1.0)
-    kk_imgs = [kk_img,kk_img_2]
+    kk_img_3 = pg.transform.rotozoom(kk_img,15,1.0)
+    kk_img_4 = pg.transform.rotozoom(kk_img,20,1.0)
+    kk_imgs = [kk_img,kk_img_2,kk_img_3,kk_img_4,kk_img]
     tmr = 0
 
     while True:
@@ -17,10 +20,11 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img,[-x,0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_imgs[tmr%2],[300,200])
+        screen.blit(bg_img_2,[1600-x, 0])
+        screen.blit(bg_img,[3200-x,0])
+        screen.blit(kk_imgs[tmr%5],[300,200])
 
         pg.display.update()
         clock.tick(100)
